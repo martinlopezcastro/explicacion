0f1d8cbea7fdefea3a628abeddc241d7ffe11d95
martin-28 <69127844+martinlopezcastro@users.noreply.github.com>
2020-10-07 21:28:19 -0300

    Add files via upload

---
+++ prueba.py
@@ -0,0 +1,12 @@
+r = input("ingresa un digito: ")
+a = input("ingresa otro digito: ")
+resultado = r+a
+print("tu resultado es", resultado)
+
+
+if resultado==6:
+	print("tu numero es 6")
+elif resultado>=7:
+	print("tu resultado es 7 o mayor a 7")
+else:
+	print("ninguna funcion se cumplio")
