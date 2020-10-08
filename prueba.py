1d98c879502de24d0efe38f192647316d4e03b5a
martin-28 <69127844+martinlopezcastro@users.noreply.github.com>
2020-10-07 21:33:44 -0300

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -6,7 +6,7 @@ print("tu resultado es", resultado)
 
 if resultado==6:
 	print("tu numero es 6")
-elif resultado>=7:
-	print("tu resultado es 7 o mayor a 7")
+elif resultado==7:
+	print("tu resultado es 7")
 else:
 	print("ninguna funcion se cumplio")
