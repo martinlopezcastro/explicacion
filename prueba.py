12528c32f1fb3e0733f3e759c3ec8cb20b497ab4
martin-28 <69127844+martinlopezcastro@users.noreply.github.com>
2020-10-29 15:02:14 -0300

    Update prueba.py

--- prueba.py
+++ prueba.py
@@ -1,12 +1,5 @@
-r = input("ingresa un digito: ")
-a = input("ingresa otro digito: ")
-resultado = r+a
-print("tu resultado es", resultado)
-
-
-if resultado==6:
-	print("tu numero es 6")
-elif resultado==7:
-	print("tu resultado es 7")
-else:
-	print("ninguna funcion se cumplio")
+r = input("ingresa tu edad: ")
+if r >= "18":
+  print("eres mayor de edad")
+elif r <= "18":
+  print("eres menor de edad")
